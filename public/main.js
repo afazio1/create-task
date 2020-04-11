fca90d25abceb1c9ed0a6123277e866ea63ec636
Alexa <alexa.fazio04@gmail.com>
2020-04-11 19:41:51 -0400

    wrote function to update the html with the weather data

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
+// Remember we can not call the API (or click the button) more than 59 times per minute
+// If you need to figure out styling, just put some fake data in the HTML.
 
-document.getElementById('weather-button').addEventListener("click", getWeather)
+
+document.getElementById('weather-button').addEventListener("click", getWeather);
+
 
 async function getWeather() {
     
@@ -7,23 +11,30 @@ async function getWeather() {
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
    
-    //console.log(json);
-    var icon = '';
-    var condition = json.weather[0].main;
+    let icon = "http://openweathermap.org/img/wn/" + json.weather[0].icon + "@2x.png";;
+    let condition = json.weather[0].main;
     let temps = [json.main.temp_min, json.main.temp, json.main.temp_max];
-    console.log(typeof temps[0]);
-    var city = json.name;
+    let city = json.name;
 
     kelvinToFahreinheit(temps);
-    
 
-    document.getElementById('weather').innerHTML = json.weather[0].main;
+    let currentWeatherData = [icon, condition, temps, city];
+
+    updateHTML(currentWeatherData);
+   
 }
 
 function kelvinToFahreinheit(temps) {
 	for (let i = 0; i < temps.length; i++) {
-		console.log(typeof temps[i]);
-		temps[i] = Math.round(((temps[i]-273.15)*1.8)+32);
+		temps[i] = Math.round(((temps[i] - 273.15) * 1.8) + 32);
 	}
 	return temps;
+}
+
+function updateHTML(currentWeatherData) {
+	document.getElementById('icon').src = currentWeatherData[0];
+	document.getElementById('condition').innerHTML = currentWeatherData[1];
+	document.getElementById('temps').innerHTML = `<span>` + currentWeatherData[2][0] + "\xB0" + `</span><span> ` + currentWeatherData[2][1] + "\xB0" + ` </span><span>` + currentWeatherData[2][2] + "\xB0" + `</span>`;
+	document.getElementById('city').innerHTML = currentWeatherData[3];
+	
 }
